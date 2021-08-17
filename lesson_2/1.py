049292f36d49504bff98a737bf0ca23d3eaace8d
Bohdan <medvedev.bogdan@gmail.com>
2021-08-17 11:00:42 +0300

    init. 1st hometask done.

---
+++ lesson_2/1.py
@@ -0,0 +1,9 @@
+list_a = ['a', 'm', 1, 14, 1.5, 11.5, False]
+print(type(list_a))
+print(type(list_a[0]))
+print(type(list_a[1]))
+print(type(list_a[2]))
+print(type(list_a[3]))
+print(type(list_a[4]))
+print(type(list_a[5]))
+print(type(list_a[6]))
