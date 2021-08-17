ba217cf29a98d365d6a16c8860d59bb5a8411340
Bohdan <medvedev.bogdan@gmail.com>
2021-08-17 19:51:26 +0300

    upgraded elem type in list

--- lesson_2/1.py
+++ lesson_2/1.py
@@ -1,9 +1,3 @@
 list_a = ['a', 'm', 1, 14, 1.5, 11.5, False]
-print(type(list_a))
-print(type(list_a[0]))
-print(type(list_a[1]))
-print(type(list_a[2]))
-print(type(list_a[3]))
-print(type(list_a[4]))
-print(type(list_a[5]))
-print(type(list_a[6]))
+for i in list_a:
+    print(type(i))
