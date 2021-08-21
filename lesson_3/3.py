0bfd18774c0ad2604899d9671bfd199f6bfab58a
Bohdan <medvedev.bogdan@gmail.com>
2021-08-21 14:05:05 +0300

    added extra lines

--- lesson_3/3.py
+++ lesson_3/3.py
@@ -2,8 +2,8 @@ arg_1 = int(input('Enter arg_1 '))
 arg_2 = int(input('Enter arg_2 '))
 arg_3 = int(input('Enter arg_3 '))
 
-def my_func (arg_1, arg_2, arg_3):
 
+def my_func (arg_1, arg_2, arg_3):
 
     if arg_2 >= arg_1 <= arg_3:
         return arg_2 + arg_3
@@ -11,5 +11,6 @@ def my_func (arg_1, arg_2, arg_3):
         return arg_1 + arg_3
     elif arg_1 >= arg_3 < arg_2:
         return arg_1 + arg_2
+
 
 print(my_func(arg_1, arg_2, arg_3))
