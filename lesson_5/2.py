993ea8214c5f0cebc72ac2af82ab92541a069ba9
Bohdan <medvedev.bogdan@gmail.com>
2021-09-05 17:45:08 +0300

    lines, words count

--- lesson_5/2.py
+++ lesson_5/2.py
@@ -1,1 +1,5 @@
-with open
+with open ('test_2.txt') as file:
+    file_lines = file.readlines()
+    for num, line in enumerate(file_lines):
+        words_count = len(line.split())
+        print(f'№ {num + 1} - {words_count} words')
